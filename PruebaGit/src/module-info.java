6c39799769a7aa0b98d89b17e0e15e9f8a98cbd5
Ivan GV <Ivan GV@DESKTOP-CENSSSI>
2020-12-20 20:42:38 -0600

    Version 1

---
+++ PruebaGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ivan GV
+ *
+ */
+module PruebaGit {
+}
---
+++ PruebaGit/src/principal/Principal.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Primer Repositorio en GITHub");
+	}
+
+}
